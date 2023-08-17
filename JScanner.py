753fc70be4aebf403482b6a8ab20cf2d20d07db2
kiwi <3279416570@qq.com>
2023-08-17 10:35:29 +0800

    去除对敏感信息的搜索 增加批量查询

--- JScanner.py
+++ JScanner.py
@@ -16,7 +16,7 @@ EXCEL_LIST = []
 def parse_args():
     """用户输入"""
     parse = argparse.ArgumentParser(description="hi 你好")
-    parse.add_argument('-u', '--url', required=True, type=str, help="输入带有http/https的网站URL")
+    parse.add_argument('-u', '--url',  type=str, help="输入带有http/https的网站URL")
     parse.add_argument('-r', '--header', type=ast.literal_eval,
                        default="{'user-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1'}",
                        help="输入user-agent,格式为\"{\'cookie\':\'xxxx\',\'user-Agent\':\'xxxx\',\'xxxx\':\'xxxx\'}\"")
@@ -30,6 +30,7 @@ def parse_args():
     parse.add_argument('-o', '--out', type=str, help="输出为Excel表格")
     parse.add_argument('-p','--proxy',type=str,help="设置代理，格式：-p xxx.xxx.xxx.xxx:端口,如果代理需要认证，格式为：username:password@xxx.xxx.xxx.xxx:xxxx")
     parse.add_argument('-d','--redup',type=str,help="需要配合-o来进行输出，有标题，状态码，返回值长度三者可以选择，选中后会对其进行去重操作，默认会对URL进行去重，不可以多选。")
+    parse.add_argument('-b','--batch',type=str,help="填入文件绝对路径，完成批量扫描，可自动去除空白行")
     return parse.parse_args()
 
 
@@ -37,7 +38,8 @@ def read(filename: str) -> list:
     """文件读取"""
     with open(filename, 'r') as file:
         lines = [line.strip().split(" ")[0] for line in file if line.strip() and line.strip()[0] != "#"]
-    return lines
+        lines = [line for line in lines if line] # 过滤空白元素
+        return lines
 
 
 def url_request(url, header, wait_time=3):
@@ -214,9 +216,9 @@ def get_title(Object):
         return title
 
 
-def write_excel(dataList):
+def write_excel(dataList,name):
     # 生成文件名（当前时间戳 + 随机数）
-    fileName = int(time.mktime(time.localtime())) + random.randint(1000, 9999)
+    fileName = name + str(random.randint(1000, 9999))
     # 创建工作簿
     workbook = xw.Workbook(str(fileName) + ".xlsx")
     # 创建子表
@@ -248,80 +250,77 @@ def write_excel(dataList):
 
     return fileName
 
-def remove_duplicates(excel_name, column_name):
+def remove_duplicates(excel_name, column_name,to_name):
+    """对指定的列进行去重"""
     # 读取Excel文件
     data = pd.read_excel(excel_name)
 
     unique_data = data.drop_duplicates(subset=column_name)
-    file_name = int(time.mktime(time.localtime())) + random.randint(1000, 9999)
+
+    file_name = to_name + str(random.randint(1000, 9999))
     # 将去重后的数据写入新的Excel文件
     unique_data.to_excel(file_name, index=False)
     # 删除源表格
     os.remove(excel_name)
 
-def sensitiveness_info(source):
-    """对敏感信息的排查"""
-    reg = "(\d{17}[\dXx])|(\d{15})|(\d{11})|([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
-    result = re.findall(source,reg)
-    return list(set(result))
-
 if __name__ == "__main__":
     args = parse_args()
-    Object = url_request(url=args.url, header=args.header, wait_time=args.wait)
-    # 此时会获取得到第一次探测url得到的信息
-    first_url_list = analysis(Object.text, args.url)
-    # 筛选出敏感信息
-    sen_info = sensitiveness_info(Object.text)
+    if args.batch:
+        url_list = read(args.batch)
+    else:
+        url_list = [args.url]
 
-    all_url_list = []
-    if args.height > 0:
-        # 假如设置了深度查找就步入
-        url_list2 = height_scan(first_url_list, header=args.header, wait_time=args.wait, high=args.height)
-        # 第二次去重，主要是为了为下面的代码减轻工作量
-        first_url_list = list(set(first_url_list + url_list2))
-    for url in first_url_list:
-        # 进行url文件路径的逐级递减
-        demo_url = decline(url, args.level)
-        # 将递减后的demo_url 放入到all_url_list列表当中
-        all_url_list.extend(demo_url)
-        # 此时会进行第三次去重，去重的是总url，主要是去除部分可能一级目录相同的问题
-    all_url_list = list(set(all_url_list))
+    for e_url in url_list:
+        Object = url_request(url=e_url, header=args.header, wait_time=args.wait)
+        # 此时会获取得到第一次探测url得到的信息
+        first_url_list = analysis(Object.text, e_url)
 
-    # 对总URL列表当中的url进行遍历，检查每一个URL的各种信息
-    for url in all_url_list:
-        try:
-            # 设置时间间隔
-            time.sleep(args.time)
-            result = url_request(url, header=args.header, wait_time=args.wait)
-            # 获取状态码
-            code = status(result)
-            # 获取返回值长度
-            out_length = return_length(result)
-            # 获得标题
-            title = get_title(result)
-        except:
-            if args.out:
-                EXCEL_LIST.append((url, "ERROR"))
-            else:
-                print(url, "----------", "\033[31mERROR\033[0m")
-        else:
-            if code in args.blackStatus:
-                pass
-            else:
+        all_url_list = []
+        if args.height > 0:
+            # 假如设置了深度查找就步入
+            url_list2 = height_scan(first_url_list, header=args.header, wait_time=args.wait, high=args.height)
+            # 第二次去重，主要是为了为下面的代码减轻工作量
+            first_url_list = list(set(first_url_list + url_list2))
+        for url in first_url_list:
+            # 进行url文件路径的逐级递减
+            demo_url = decline(url, args.level)
+            # 将递减后的demo_url 放入到all_url_list列表当中
+            all_url_list.extend(demo_url)
+            # 此时会进行第三次去重，去重的是总url，主要是去除部分可能一级目录相同的问题
+        all_url_list = list(set(all_url_list))
+
+        # 对总URL列表当中的url进行遍历，检查每一个URL的各种信息
+        for url in all_url_list:
+            try:
+                # 设置时间间隔
+                time.sleep(args.time)
+                result = url_request(url, header=args.header, wait_time=args.wait)
+                # 获取状态码
+                code = status(result)
+                # 获取返回值长度
+                out_length = return_length(result)
+                # 获得标题
+                title = get_title(result)
+            except:
                 if args.out:
-                    # 将所有的数据进行存储，然后写入Excel
-                    EXCEL_LIST.append((url, code, out_length, title))
+                    EXCEL_LIST.append((url, "ERROR"))
                 else:
-                    print("\033[34m", url,"\033[0m", "----------", code, "---------", "\033[33m", out_length, "\033[0m", "----------", "\033[32m", title, "\033[0m")
-                if code == 200:
-                    sen_info.extend(sensitiveness_info(result.text))
+                    print(url, "----------", "\033[31mERROR\033[0m")
+            else:
+                if code in args.blackStatus:
+                    pass
+                else:
+                    if args.out:
+                        # 将所有的数据进行存储，然后写入Excel
+                        EXCEL_LIST.append((url, code, out_length, title))
+                    else:
+                        print("\033[34m", url,"\033[0m", "----------", code, "---------", "\033[33m", out_length, "\033[0m", "----------", "\033[32m", title, "\033[0m")
 
-        for info in sen_info:
-            print("\033[0;37;40m",info,"\033[0m")
-
-    # 用户选中了要以Excel的形式输出
-    if args.out:
-        filename = write_excel(EXCEL_LIST)
-        if args.redup:
-            # 用户自定义去重的列
-            remove_duplicates(filename,args.redup)
+        # 用户选中了要以Excel的形式输出
+        if args.out:
+            name = e_url.replace(':','_')
+            name = name.replace('/','_')
+            filename = write_excel(EXCEL_LIST,name)
+            if args.redup:
+                # 用户自定义去重的列
+                remove_duplicates(filename,args.redup,name)
